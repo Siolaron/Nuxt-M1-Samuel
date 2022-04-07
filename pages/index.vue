95ae0f980d98e8ef69ca968cf022e4785b845339
Siolaron <45102078+Siolaron@users.noreply.github.com>
2022-04-07 18:12:17 +0200

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Annonces />
+  
+  </div>
+</template>
+
+
+<script>
+import Compteur from "../components/Compteur.vue";
+export default {
+  components: { Compteur },
+  name: "IndexPage",
+};
+</script>
---
+++ pages/realestates/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>Title: {{ title }}</p>
+    <p>Description: {{ description }}</p>
+    <p>Surface: {{ surface }}</p>
+    <p>Nombre de chambres: {{ rooms }}</p>
+    <p>Prix: {{ price }}</p>
+    <p>Meublé : {{meuble}}</p>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData({ params }) {
+    const realestate = await fetch("http://localhost:1337/api/real-estates/" + params.id).then((res) =>
+      res.json()
+    );
+    return {
+      title: realestate.data.attributes.Title,
+      description: realestate.data.attributes.Description,
+      surface: realestate.data.attributes.Area,
+      rooms: realestate.data.attributes.Number_of_rooms,
+      price: realestate.data.attributes.Price + " €",
+      meuble : realestate.data.attributes.Furnished
+    }
+  },
+};
+</script>
